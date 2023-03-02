654053f7d952a34d64756ce7aca45da90b3a6795
Cleverson <cleverson.silva@msn.com>
2023-03-02 12:10:30 -0400

    adicionado comentário que identifica a Lista de Exercício e a Questão

--- LE03_Q01_Rev01.java
+++ LE03_Q01_Rev01.java
@@ -1,3 +1,7 @@
+/*
+LISTA DE EXERCÍCIOS 03
+QUESTÃO 01 - Utilizando a estrutura de controle para repetiçãowhile, exiba 20 vezes a mensagem “Eu gosto de estudar Algoritmos!”.
+ */
 public class LE03_Q01_Rev01 {
 	public static void main(String[] args) {
 		int cont = 1;

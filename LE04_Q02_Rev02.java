b42e19c995e5bd0258c8ea8b38c79a656973f327
Cleverson <cleverson.silva@msn.com>
2023-03-06 12:38:47 -0400

    upDate Inicial

--- LE04_Q02_Rev02.java
+++ LE04_Q02_Rev02.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LE04_Q02_Rev01 {
+public class LE04_Q02_Rev02 {
 
 	public static void main(String[] args) {
 		int num = 0;
@@ -8,41 +8,52 @@ public class LE04_Q02_Rev01 {
 		
 		Scanner lerNum = new Scanner(System.in);
 		
-		System.out.println("Informe um Número entre 1 e 10: ");
+		System.out.print("Informe um Número entre 1 e 10: ");
 		num = lerNum.nextInt();
 		
-		if(num == 1) {
+		switch(num) {
+		case 1:
 			numExtenso = "um.";
 			System.out.println("\n Número por extenso: " + numExtenso);
-		} else if(num == 2) {
+			break;
+		case 2:
 			numExtenso = "dois.";
 			System.out.println("\n Número por extenso: " + numExtenso);
-		} else if(num == 3) {
+			break;
+		case 3:
 			numExtenso = "três.";
 			System.out.println("\n Número por extenso: " + numExtenso);
-		} else if(num == 4) {
+			break;
+		case 4:
 			numExtenso = "quatro.";
 			System.out.println("\n Número por extenso: " + numExtenso);
-		} else if(num == 5) {
+			break;
+		case 5:
 			numExtenso = "cinco.";
 			System.out.println("\n Número por extenso: " + numExtenso);
-		} else if(num == 6) {
+			break;
+		case 6:
 			numExtenso = "seis.";
 			System.out.println("\n Número por extenso: " + numExtenso);
-		} else if(num == 7) {
+			break;
+		case 7:
 			numExtenso = "sete.";
 			System.out.println("\n Número por extenso: " + numExtenso);
-		} else if(num == 8) {
+			break;
+		case 8:
 			numExtenso = "oito.";
 			System.out.println("\n Número por extenso: " + numExtenso);
-		} else if(num == 9) {
+			break;
+		case 9:
 			numExtenso = "nove.";
 			System.out.println("\n Número por extenso: " + numExtenso);
-		} else if(num == 10) {
+			break;
+		case 10:
 			numExtenso = "dez.";
 			System.out.println("\n Número por extenso: " + numExtenso);
-		}else if((num <=0) && (num >10)) {
-			System.out.println("Número informado não atende ao solicitado.");
+			break;
+			default:
+				System.out.println("Número informado não atende ao solicitado.");
 		}
 
 	}
